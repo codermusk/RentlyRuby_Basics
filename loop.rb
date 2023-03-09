bdff2fdabf5b086b88b3428a1e1840912a53e712
bharath <bharathbk1221@gmail.com>
2023-03-09 17:56:38 +0530

    Learnt Classes Objects and Functions and arrays

---
+++ loop.rb
@@ -0,0 +1,9 @@
+for i in[1,2,3]
+  p i
+end
+
+[1,2,3].each { |i| p i }
+for i in [1,2,4,4,5] do p i end
+
+[1,2,3].each do |i| p i end
+
---
+++ whileLoops.rb
@@ -0,0 +1,7 @@
+x = 10
+while x>0
+  p x
+  x-=1
+end
+
+print "bharath"
