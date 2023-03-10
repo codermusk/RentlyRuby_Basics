737a4daa60716590b5c0216d5223554e0d6206be
bharath <bharathbk1221@gmail.com>
2023-03-10 17:48:18 +0530

    sort

--- bang.rb
+++ bang.rb
@@ -1,5 +1,6 @@
+
 friend = %w[hello how are you]
-friend.map! do |friend|
+friend.map do |friend|
   friend.upcase
 end
 
@@ -7,3 +8,13 @@ for i in friend
   p i
 end
 #if we want to change the data in the map or array or string we need to use bangs in order to change the data
+# if we dont want to use the bangs we can use methods in enumerables in order to change the data
+def bangs friend
+  friend.select!{|i|
+    i!="hello"
+  }
+end
+ bangs(friend)
+for i in friend
+  p i
+end
---
+++ sort.rb
@@ -0,0 +1,9 @@
+nums = [1,24,5,454 ,62 , 3,345 , 22]
+nums.sort!
+for i in nums
+  p i
+end
+
+# do
+#
+# end
