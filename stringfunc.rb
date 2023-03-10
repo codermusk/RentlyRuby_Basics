714037573ee3ee5d1bf86c12fb2a4cd2e8cfcfc7
bharath <bharathbk1221@gmail.com>
2023-03-10 14:21:18 +0530

    Str Funcs

--- stringfunc.rb
+++ stringfunc.rb
@@ -3,7 +3,7 @@ friend.each { |str|  p str.upcase}
 fruits = ["apple", "banana", "strawberry", "pineapple"]
 fruits.each_with_index { |fruit, index| puts fruit if index.even? }
 friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
-
+numbers = [5,6,7,8,9]
 friends.map! { |friend| friend.upcase }
 
 friends.each { |idx| p idx }
@@ -15,4 +15,19 @@ friends.each do |friend|
   if friend!="Brian"
     p friend
   end
+end
+
+numbers.map! do |sum |
+  p sum
+
+end
+
+numbers.reduce do |num , i|
+  num+i
+  p i
+
+end
+numbers.reduce(1000) do |sum , i|
+  sum+i
+
 end
