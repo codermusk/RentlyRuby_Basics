b50e187e2b977b7405d90cc32727d646b373463c
bharath <bharathbk1221@gmail.com>
2023-03-13 14:06:32 +0530

    set

---
+++ revDivMod.rb
@@ -0,0 +1,15 @@
+LIMIT = 1<<31
+def reverse(x)
+  x = -x if neg = x.negative?
+  res = 0
+  until x.zero?
+    x,b = x.divmod(10)
+    res = res*10+b
+
+  end
+  return 0 if res >= LIMIT
+  neg ? -res : res
+end
+
+p reverse -103453434
+
