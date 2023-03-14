acadd58f3d0913dd0d7d5736d9e989732ba3d7f1
bharath <bharathbk1221@gmail.com>
2023-03-14 10:13:43 +0530

    string

--- reverseString.rb
+++ reverseString.rb
@@ -1,2 +1,20 @@
 str ="hello world "
 p str.reverse!
+
+number = 1002120
+p number.to_s.reverse!
+
+
+alpha = Hash.new
+
+alpha[1] = 10
+alpha[2] = 20
+alpha.each do |i , j|
+  p i
+  p j
+end
+
+# alpha.each do |i , j|
+#   p i
+#   p j
+# end
